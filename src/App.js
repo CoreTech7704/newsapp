d9a063d958056254fe1babeddd16fe7be3e1bbd4
Sarvam Patel <148036885+CoreTech7704@users.noreply.github.com>
2025-06-16 21:53:13 +0530

    rcc build

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 import React, { Component } from 'react'
@@ -7,7 +6,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        
+        Hello my component
       </div>
     )
   }
