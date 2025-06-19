c84260987a8dbd09c3d3f3113eaeffee7620aa94
Sarvam Patel <148036885+CoreTech7704@users.noreply.github.com>
2025-06-19 20:08:29 +0530

    News upadate

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 
 export class NewsItem extends Component {
+
   render() {
-    let { title, description, mode } = this.props;
+    let { title, description, mode, imageurl } = this.props;
     let cardClass = mode === 'dark' ? 'bg-dark text-white' : '';
 
     return (
-      <div>
+      <div className='my-3'>
         <div className={`card ${cardClass}`} style={{ width: "18rem" }}>
-          <img src="https://static.vecteezy.com/system/resources/thumbnails/006/299/370/original/world-breaking-news-digital-earth-hud-rotating-globe-rotating-free-video.jpg" className="card-img-top" alt="news" />
+          <img src={imageurl} className="card-img-top" alt="news" />
           <div className="card-body">
             <h5 className="card-title">{title}</h5>
             <p className="card-text">{description}</p>
-            <a href="/newsdetail" className="btn btn-primary">Go somewhere</a>
+            <a href="/newsdetail" className="btn btn-sm btn-primary">Read More</a>
           </div>
         </div>
       </div>
