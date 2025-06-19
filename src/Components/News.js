b9f50272b61907b009a8106bdb6ef15f22ec6732
Sarvam Patel <148036885+CoreTech7704@users.noreply.github.com>
2025-06-19 20:35:33 +0530

    News Optimization

--- src/Components/News.js
+++ src/Components/News.js
@@ -28,6 +28,19 @@ export class News extends Component {
       "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
       "publishedAt": "2020-03-30T15:26:05Z",
       "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
+    },
+    {
+      "source": {
+        "id": "espn-cric-info",
+        "name": "ESPN Cric Info"
+      },
+      "author": null,
+      "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
+      "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
+      "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
+      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
+      "publishedAt": "2020-03-30T15:26:05Z",
+      "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
     }
   ]
     constructor(){
@@ -42,17 +55,12 @@ export class News extends Component {
       <div className='container my-3'>
         <h2>SnapNews Top Headlines</h2>
         <div className="row">
-          <div className="col-md-4">
-            <NewsItem mode={this.props.mode} title="myTitle" description="mydesc" imageurl="https://static.vecteezy.com/system/resources/thumbnails/006/299/370/original/world-breaking-news-digital-earth-hud-rotating-globe-rotating-free-video.jpg" newsurl="TODO"/>
-          </div>
-          <div className="col-md-4">
-            <NewsItem mode={this.props.mode} title="myTitle" description="mydesc" imageurl="https://static.vecteezy.com/system/resources/thumbnails/006/299/370/original/world-breaking-news-digital-earth-hud-rotating-globe-rotating-free-video.jpg" newsurl="TODO"/>
-          </div>
-          <div className="col-md-4">
-            <NewsItem mode={this.props.mode} title="myTitle" description="mydesc" imageurl="https://static.vecteezy.com/system/resources/thumbnails/006/299/370/original/world-breaking-news-digital-earth-hud-rotating-globe-rotating-free-video.jpg" newsurl="TODO"/>
-          </div>
+          {this.state.articles.map((element)=>{
+            return<div className="col-md-4" key={element.url}>
+              <NewsItem title={element.title.slice(0,45)} mode={this.props.mode} description={element.description.slice(0,95)} imageurl={element.urlToImage} newsurl={element.url}/>
+            </div>
+          })}
         </div>
-        
       </div>
     )
   }
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -11,9 +11,9 @@ export class NewsItem extends Component {
         <div className={`card ${cardClass}`} style={{ width: "18rem" }}>
           <img src={imageurl} className="card-img-top" alt="news" />
           <div className="card-body">
-            <h5 className="card-title">{title}</h5>
-            <p className="card-text">{description}</p>
-            <a href="/newsdetail" className="btn btn-sm btn-primary">Read More</a>
+            <h5 className="card-title">{title}...</h5>
+            <p className="card-text">{description}...</p>
+            <a href={newsurl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
           </div>
         </div>
       </div>
