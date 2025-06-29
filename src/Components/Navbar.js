09239e61309312a56fcce58b490d5476c8768a76
Sarvam Patel <148036885+CoreTech7704@users.noreply.github.com>
2025-06-29 20:54:55 +0530

    converted navbar.js classbased componets to fuction based components

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,31 +1,27 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import {Link} from "react-router-dom";
+import { Link } from 'react-router-dom';
 
-export class Navbar extends Component {
-  static propTypes = {
-    mode: PropTypes.string
-  };
+const Navbar = ({ mode, ToggleMode }) => {
+  return (
+    <nav className={`navbar navbar-expand-lg navbar-${mode} bg-${mode}`}>
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">SnapNews</Link>
 
-  render() {
-    return (
-      <div>
-        <nav className={`navbar navbar-expand-lg navbar-${this.props.mode} bg-${this.props.mode}`}>
-          <div className="container-fluid">
-            <Link className="navbar-brand" to="/">SnapNews</Link>
-            <button
-              className="navbar-toggler"
-              type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarSupportedContent"
-              aria-controls="navbarSupportedContent"
-              aria-expanded="false"
-              aria-label="Toggle navigation"
-            >
-              <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <Link className="nav-link active" aria-current="page" to="/">Home</Link>
             </li>
@@ -42,15 +38,14 @@ export class Navbar extends Component {
               >
                 Categories
               </Link>
-              <ul className={`dropdown-menu dropdown-menu-${this.props.mode}`} aria-labelledby="navbarDropdown">
-              <li><Link className="dropdown-item" to="/business">Business</Link></li>
-              <li><Link className="dropdown-item" to="/entertainment">Entertainment</Link></li>
-              <li><Link className="dropdown-item" to="/health">Health</Link></li>
-              <li><Link className="dropdown-item" to="/science">Science</Link></li>
-              <li><Link className="dropdown-item" to="/sports">Sports</Link></li>
-              <li><Link className="dropdown-item" to="/technology">Technology</Link></li>
-            </ul>
-
+              <ul className={`dropdown-menu dropdown-menu-${mode}`} aria-labelledby="navbarDropdown">
+                <li><Link className="dropdown-item" to="/business">Business</Link></li>
+                <li><Link className="dropdown-item" to="/entertainment">Entertainment</Link></li>
+                <li><Link className="dropdown-item" to="/health">Health</Link></li>
+                <li><Link className="dropdown-item" to="/science">Science</Link></li>
+                <li><Link className="dropdown-item" to="/sports">Sports</Link></li>
+                <li><Link className="dropdown-item" to="/technology">Technology</Link></li>
+              </ul>
             </li>
 
             <li className="nav-item">
@@ -58,26 +53,28 @@ export class Navbar extends Component {
             </li>
           </ul>
 
-              {/* Toggle Button for Dark/Light Mode */}
-              <div className={`form-check form-switch text-${this.props.mode === 'light' ? 'dark' : 'light'}`}>
-                <input
-                  className="form-check-input"
-                  type="checkbox"
-                  role="switch"
-                  id="modeSwitch"
-                  onClick={this.props.ToggleMode}
-                />
-                <label className="form-check-label" htmlFor="modeSwitch">
-                  Enable {this.props.mode === 'light' ? 'Dark' : 'Light'} Mode
-                </label>
-              </div>
-
-            </div>
+          {/* Dark/Light Mode Toggle */}
+          <div className={`form-check form-switch text-${mode === 'light' ? 'dark' : 'light'}`}>
+            <input
+              className="form-check-input"
+              type="checkbox"
+              role="switch"
+              id="modeSwitch"
+              onClick={ToggleMode}
+            />
+            <label className="form-check-label" htmlFor="modeSwitch">
+              Enable {mode === 'light' ? 'Dark' : 'Light'} Mode
+            </label>
           </div>
-        </nav>
+        </div>
       </div>
-    );
-  }
-}
+    </nav>
+  );
+};
+
+Navbar.propTypes = {
+  mode: PropTypes.string.isRequired,
+  ToggleMode: PropTypes.func.isRequired,
+};
 
 export default Navbar;
